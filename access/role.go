e6de9637b2fe697bef1e6ae7776cdc6534344c5c
Alan Bunjevac <alan.bunjevac@gmail.com>
2024-04-02 13:52:28 +0200

    Roles

---
+++ access/role.go
@@ -0,0 +1,53 @@
+package access
+
+import "strconv"
+
+// Role represents the role of a user.
+// The role is used to determine the access level of the user.
+type Role int
+
+// Role enumeration.
+const (
+	RoleNone Role = iota
+	RoleAdmin
+	RoleCreator
+	RoleEditor
+	RoleGuest
+)
+
+// AllRoles is a list of all roles. Used for testing purposes to validate that all
+// enum values are covered.
+//
+//nolint:gochecknoglobals
+var AllRoles = []Role{
+	RoleNone,
+	RoleAdmin,
+	RoleCreator,
+	RoleEditor,
+	RoleGuest,
+}
+
+// Has checks if the current role contains the given role.
+// Use this method instead of == to allow for future upgrades to combined roles
+// using bitwise operations.
+func (r Role) Has(role Role) bool {
+	return r&role == role
+}
+
+// String returns the string representation of the role.
+func (r Role) String() string {
+	switch r {
+	case RoleNone:
+		return "none"
+	case RoleAdmin:
+		return "admin"
+	case RoleCreator:
+		return "creator"
+	case RoleEditor:
+		return "editor"
+	case RoleGuest:
+		return "guest"
+	}
+
+	return "Role(" + strconv.Itoa(int(r)) + ")"
+}
---
+++ access/role_test.go
@@ -0,0 +1,35 @@
+package access
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestRole_Has(t *testing.T) {
+	t.Parallel()
+
+	for _, r := range AllRoles {
+		t.Run(r.String(), func(t *testing.T) {
+			require.True(t, r.Has(r))
+		})
+	}
+}
+
+func TestRole_String(t *testing.T) {
+	t.Parallel()
+
+	for _, r := range AllRoles {
+		t.Run(r.String(), func(t *testing.T) {
+			require.NotEmpty(t, r.String())
+			require.False(t, strings.HasPrefix(r.String(), "Role("))
+		})
+	}
+
+	t.Run("unknown", func(t *testing.T) {
+		r := Role(100)
+
+		require.Equal(t, "Role(100)", r.String())
+	})
+}
